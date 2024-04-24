e8cf9ba087659339528221214367fac9d8112d28
irenee <iradukundairenee9@gmail.com>
2024-04-24 23:14:45 +0200

    deploy render

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import {lazy } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import 'leaflet/dist/leaflet.css';
+const Home = lazy(() => import('./pages/Home'))
+const Location = lazy(() => import('./pages/Location'))
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="location/:roomId" element={<Location />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ src/config/index.ts
@@ -0,0 +1,1 @@
+export const SOCKET_URL = 'https://locshare-xc0v.onrender.com'
